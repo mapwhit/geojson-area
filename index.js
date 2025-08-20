66de51dda4a09985b7a8e4cd9cd43a78d0575770
Damian Krzeminski <pirxpilot@furkot.com>
2025-08-20 10:04:04 -0600

    use biome as a linter and formatter

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ module.exports.geometry = geometry;
 module.exports.ring = ringArea;
 
 function geometry({ type, coordinates, geometries }) {
-    switch (type) {
-        case 'Polygon':
-            return polygonArea(coordinates);
-        case 'MultiPolygon':
-            return coordinates.reduce((area, p) => area + polygonArea(p), 0);
-        case 'Point':
-        case 'MultiPoint':
-        case 'LineString':
-        case 'MultiLineString':
-            return 0;
-        case 'GeometryCollection':
-            return geometries.reduce((area, g) => area + geometry(g), 0);
-    }
+  switch (type) {
+    case 'Polygon':
+      return polygonArea(coordinates);
+    case 'MultiPolygon':
+      return coordinates.reduce((area, p) => area + polygonArea(p), 0);
+    case 'Point':
+    case 'MultiPoint':
+    case 'LineString':
+    case 'MultiLineString':
+      return 0;
+    case 'GeometryCollection':
+      return geometries.reduce((area, g) => area + geometry(g), 0);
+  }
 }
 
 function polygonArea(coords) {
-    let area = 0;
-    if (coords && coords.length > 0) {
-        area += Math.abs(ringArea(coords[0]));
-        for (let i = 1; i < coords.length; i++) {
-            area -= Math.abs(ringArea(coords[i]));
-        }
+  let area = 0;
+  if (coords?.length > 0) {
+    area += Math.abs(ringArea(coords[0]));
+    for (let i = 1; i < coords.length; i++) {
+      area -= Math.abs(ringArea(coords[i]));
     }
-    return area;
+  }
+  return area;
 }
 
 /**
@@ -45,33 +45,41 @@ function polygonArea(coords) {
  *     meters.
  */
 
+const RADIUS_SQR = wgs84.RADIUS ** 2;
+
 function ringArea(coords) {
-    // it's a ring - ignore the last one
-    const len = coords.length - 1;
+  // it's a ring - ignore the last one
+  const len = coords.length - 1;
 
-    if (len < 3) {
-        return 0;
-    }
+  if (len < 3) {
+    return 0;
+  }
 
-    let total = 0;
+  let total = 0;
 
-    // start from [len - 2, len - 1, 0]
-    let l_x = rad(coords[len - 2][0]), l_y = rad(coords[len - 2][1]);
-    let m_x = rad(coords[len - 1][0]), m_y = rad(coords[len - 1][1]);
-    let u_x = 0, u_y = 0;
+  // start from [len - 2, len - 1, 0]
+  let l_x = rad(coords[len - 2][0]);
+  const m = coords[len - 1];
+  let m_x = rad(m[0]);
+  let m_y = rad(m[1]);
+  let u_x = 0;
 
-    for (let i = 0; i < len; i++) {
-        u_x = rad(coords[i][0]); u_y = rad(coords[i][1]);
+  for (let i = 0; ; i++) {
+    const c = coords[i];
+    u_x = rad(c[0]);
 
-        total += ( u_x - l_x ) * Math.sin(m_y);
+    total += (u_x - l_x) * Math.sin(m_y);
 
-        l_x = m_x; l_y = m_y;
-        m_x = u_x; m_y = u_y;
-    }
+    if (i === len - 1) break;
 
-    return total * wgs84.RADIUS * wgs84.RADIUS / 2;
+    l_x = m_x;
+    m_x = u_x;
+    m_y = rad(c[1]);
+  }
+
+  return (total * RADIUS_SQR) / 2;
 }
 
-function rad(_) {
-    return _ * Math.PI / 180;
+function rad(n) {
+  return (n * Math.PI) / 180;
 }
--- test/basic.js
+++ test/basic.js
@@ -5,26 +5,38 @@ const gjArea = require('../');
 const ill = require('./illinois.json');
 const all = require('./all.json');
 
-test('geojson area', async function (t) {
-  await t.test('computes the area of illinois', function () {
+test('geojson area', async t => {
+  await t.test('computes the area of illinois', () => {
     // 145978332359.36746 - round to 1/1000th part for comparison
     assert.equal(Math.round(1000 * gjArea.geometry(ill)), 145978332359367);
   });
 
   // http://www.wolframalpha.com/input/?i=surface+area+of+earth
-  await t.test('computes the area of the world', function () {
-    assert.equal(gjArea.geometry(all), 511207893395811.06);
+  await t.test('computes the area of the world', () => {
+    assert.equal(gjArea.geometry(all), 511207893395811);
   });
 
-  await t.test('point has zero area', function () {
+  await t.test('point has zero area', () => {
     assert.equal(gjArea.geometry({ type: 'Point', coordinates: [0, 0] }), 0);
   });
 
-  await t.test('linestring has zero area', function () {
-    assert.equal(gjArea.geometry({ type: 'LineString', coordinates: [[0, 0], [1, 1]] }), 0);
+  await t.test('linestring has zero area', () => {
+    assert.equal(
+      gjArea.geometry({
+        type: 'LineString',
+        coordinates: [
+          [0, 0],
+          [1, 1]
+        ]
+      }),
+      0
+    );
   });
 
-  await t.test('geometrycollection is the sum', function () {
-    assert.equal(gjArea.geometry({ type: 'GeometryCollection', geometries: [all, ill] }), 511353871728170.44);
+  await t.test('geometrycollection is the sum', () => {
+    assert.equal(
+      gjArea.geometry({ type: 'GeometryCollection', geometries: [all, ill] }),
+      511353871728170.4
+    );
   });
 });
